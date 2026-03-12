65141cad513df22ef76122eb9cb134dd5c5e8a22
esplusnin <esplusnin94@yandex.ru>
2026-03-12 15:27:26 +0300

    Initial Commit

---
+++ Chicken#2/Chicken_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Chicken_2App.swift
+//  Chicken#2
+//
+//  Created by Евгений on 12.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Chicken_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
